d43dcc088ebb4067cf9ce780b241ce32632296a0
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-04-23 16:44:11 +0900

    CRUD

---
+++ myapp/app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(params.require(:post).permit(:name, :content, :place))
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:content, :place))
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+end
